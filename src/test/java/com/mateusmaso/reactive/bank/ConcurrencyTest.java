ffbfe2b09ee978472bfa7970e521e5dcbbb433c5
Mateus Maso <m.maso25@gmail.com>
2019-04-23 22:29:46 -0300

    Add failing concurrency test

--- src/test/java/com/mateusmaso/reactive/bank/ConcurrencyTest.java
+++ src/test/java/com/mateusmaso/reactive/bank/ConcurrencyTest.java
@@ -8,12 +8,14 @@ import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import money.MonetaryAmount;
 import money.account.Account;
+import money.account.AccountOperation;
 import money.account.AccountService;
 import money.exceptions.InsufficientFundsException;
 import money.transaction.Transaction;
@@ -23,6 +25,8 @@ import money.inmemory.*;
 public class ConcurrencyTest {
   private AccountService accountService;
   private InMemoryStore store;
+  private InMemoryAccountOperation accountOperation;
+  private InMemoryAccountRepository accountRepository;
   private VerySlowInMemoryTransactionRepository transactionRepository;
 
   class VerySlowInMemoryTransactionRepository extends InMemoryTransactionRepository {
@@ -47,17 +51,24 @@ public class ConcurrencyTest {
     }
   }
 
+  class DummyUnsafeOperation implements AccountOperation {
+    @Override
+    public <V> V operateOn(Account account, Supplier<V> operation) {
+      return operation.get();
+    }
+  }
+
   @Before
   public void setUp() {
     this.store = new InMemoryStore();
-    InMemoryAccountOperation accountOperation = new InMemoryAccountOperation();
-    InMemoryAccountRepository accountRepository = new InMemoryAccountRepository(store);
+    this.accountOperation = new InMemoryAccountOperation();
+    this.accountRepository = new InMemoryAccountRepository(store);
     this.transactionRepository = new VerySlowInMemoryTransactionRepository(store, 0);
-    this.accountService = new AccountService(accountOperation, accountRepository, this.transactionRepository);
+    this.accountService = new AccountService(this.accountOperation, this.accountRepository, this.transactionRepository);
   }
 
   @Test
-  public void itShouldNotInsertTwoTransferTransactionsWhenTheresConcurrency() throws InterruptedException {
+  public void itShouldNotInsertTwoTransferTransactionsWhenThereIsConcurrencyControl() throws InterruptedException {
     Account fromAccount = this.accountService.createAccount(Currency.getInstance("USD")).join();
     Account toAccount = this.accountService.createAccount(Currency.getInstance("USD")).join();
 
@@ -98,6 +109,54 @@ public class ConcurrencyTest {
 
     assertEquals(
       2, 
+      this.store.getTransactionEntriesByAccountId().get(fromAccount.getId()).size()
+    );
+  }
+
+  @Test
+  public void itShouldWronglyInsertTwoTransferTransactionsWhenThereIsNotConcurrencyControl() throws InterruptedException {
+    this.accountService = new AccountService(new DummyUnsafeOperation(), this.accountRepository, this.transactionRepository);
+
+    Account fromAccount = this.accountService.createAccount(Currency.getInstance("USD")).join();
+    Account toAccount = this.accountService.createAccount(Currency.getInstance("USD")).join();
+
+    this.accountService.load(fromAccount.getId(), MonetaryAmount.usd(BigDecimal.TEN)).join();
+
+    Thread t1 = new Thread(new Runnable() {
+      public void run() {
+        transactionRepository.setWaitTime(5);
+        accountService
+          .transfer(fromAccount.getId(), toAccount.getId(), MonetaryAmount.usd(BigDecimal.TEN))
+          .whenComplete(
+            (result, exception) -> {
+              assertNull(exception);
+              assertNotNull(result);
+            }
+          );
+      }
+    });
+
+    t1.start();
+
+    Thread t2 = new Thread(new Runnable() {
+      public void run() {
+        transactionRepository.setWaitTime(0);
+        accountService
+          .transfer(fromAccount.getId(), toAccount.getId(), MonetaryAmount.usd(BigDecimal.TEN))
+          .whenComplete(
+            (result, exception) -> {
+              assertNull(exception);
+              assertNotNull(result);
+            }
+          );
+      }
+    });
+
+    t2.start();
+    TimeUnit.SECONDS.sleep(2);
+
+    assertEquals(
+      3, 
       this.store.getTransactionEntriesByAccountId().get(fromAccount.getId()).size()
     );
   }
